58a8c63fdb3d4d917b69d25a4932aad7f3b2bfc8
Maciej Pałubicki <palubickimaciej5@gmail.com>
2022-10-16 13:15:16 +0200

    Adding Basic Look

--- ArduinoThermometer/MainPage.xaml.cs
+++ ArduinoThermometer/MainPage.xaml.cs
@@ -8,18 +8,13 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-		CounterBtn.Text = String.Empty;
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count+=2;
 
-		for (int i = 0; i < count; i++)
-		{
-			CounterBtn.Text.Substring(0, i).ToUpper();
-		}
-		SemanticScreenReader.Announce(CounterBtn.Text);
+	
 	}
 }
 
--- ArduinoThermometer/MauiProgram.cs
+++ ArduinoThermometer/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace ArduinoThermometer;
+﻿using Syncfusion.Maui.Core.Hosting;
+
+namespace ArduinoThermometer;
 
 public static class MauiProgram
 {
@@ -7,7 +9,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .ConfigureSyncfusionCore()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
